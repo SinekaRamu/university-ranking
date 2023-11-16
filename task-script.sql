bd82c8ad78b982ba7ceb7be75334194f140ce807
Sineka <100013918+SinekaRamu@users.noreply.github.com>
2023-11-16 23:30:56 +0530

    Create task-script.sql

---
+++ task-script.sql
@@ -0,0 +1,13 @@
+-- task 2
+
+-- 1. get students count college wise
+select 	count(stid) from students where college = 6;
+
+-- 2. get students count in a college, course wise
+-- 3. get the university rank holder across all courses(1 student)
+-- 4. get the list of rank holders each course
+-- 5. get the college topper across all courses
+-- 6. get the college toppers each course
+-- 7. get the failed students count each subject 
+-- 8. get over all students list with semester marks
+-- 9. get the student list who wasnt appear to the exams
