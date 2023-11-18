f8463630be8ecdfd89be2c2a83cec93f33db38b9
Sineka <100013918+SinekaRamu@users.noreply.github.com>
2023-11-18 22:56:49 +0530

    Task 2

--- task-script.sql
+++ task-script.sql
@@ -8,6 +8,14 @@ on s.college = c.college_id
 group by course_name;
 
 -- 2. get students count in a college, course wise
+select c.college_name, cr.course_name, count(s.stid)
+from colleges s 
+inner join college_courses cc on c.college_id = cc.college_id
+inner join courses cr on cr.course_id = cc.course_id
+left join students s on cc.college_id = s.college and cr.course_id = s.course
+group by c. college_name, cr.course_name having count (s.stid) > 0
+order by c.college_name, student_count desc;
+
 -- 3. get the university rank holder across all courses(1 student)
 -- 4. get the list of rank holders each course
 -- 5. get the college topper across all courses
