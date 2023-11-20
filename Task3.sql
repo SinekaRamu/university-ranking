ea99f8ed470ab1ec6dfb4a54c5ee50aed37341c0
Sineka <100013918+SinekaRamu@users.noreply.github.com>
2023-11-20 23:04:55 +0530

    Create Task3.sql

---
+++ Task3.sql
@@ -0,0 +1,7 @@
+-- task 3
+-- 1. update the mark to 40 those who were scored the marks between 35 to 39
+-- 2. get the history of marks that are changed
+-- 3. choose any select query from tast 2 and insert the values into a temp table(https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-select-into/)
+-- 4. delete a college and its respective things
+-- 5. alter all the tables add audit columns (createdAt,createBy,updatedAt,updatedBy)
+-- 6. remove the duplicate values in the mark table(insert values for your convenient)
