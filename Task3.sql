334355aaacccb8e095c4e045f29bbdd8b2de4517
Sineka <100013918+SinekaRamu@users.noreply.github.com>
2023-11-20 23:06:07 +0530

    Update Between

--- Task3.sql
+++ Task3.sql
@@ -1,5 +1,9 @@
 -- task 3
 -- 1. update the mark to 40 those who were scored the marks between 35 to 39
+update exam_marks 
+set marks = 40
+where marks between 35 and 39;
+
 -- 2. get the history of marks that are changed
 -- 3. choose any select query from tast 2 and insert the values into a temp table(https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-select-into/)
 -- 4. delete a college and its respective things
